e6f202d597d81b064b8b8b6c55dce344ef9d06c4
rdvelazquez <ryan.velazquez@azimuth1.com>
2020-04-10 11:31:51 -0400

    merge issues and citations data on DOIs

--- litsearch/getInternalData.py
+++ litsearch/getInternalData.py
@@ -4,6 +4,9 @@ import json
 import base64
 
 
+headers = {} # For Development you can insert your GH api token here and up the rate limit {'Authorization': 'token %s' % "<apiToken>"}
+
+
 # Issues Helper Functions
 def getIssuesFromAPI():
     """ Gets all the issues and pull-requests (GH treats PRs like issues in the api)
@@ -15,7 +18,7 @@ def getIssuesFromAPI():
     while numberOfIssuesReturned != 0:
         issuesResponse = requests.get(
             "https://api.github.com/repos/greenelab/covid19-review/issues?state=all&per_page=50&page=" +
-            str(pageNumber))
+            str(pageNumber), headers=headers)
         issues_page = json.loads(issuesResponse.text)
         issues = issues + issues_page
         numberOfIssuesReturned = len(issues_page)
@@ -46,6 +49,7 @@ def getCitationFromIssue(issue):
         return "unknown"
 
 
+# TODO: see if we can use Manubot for this
 def getPaperTitleFromIssue(issue):
     """ gets the papers title from the issue title """
     try:
@@ -86,7 +90,7 @@ def getDOIFromCitation(citation):
         return "unknown"
 
 
-def getIssuesDF(issues):
+def getIssuesDF(issues, removeDuplicates=True):
     """ takes a list of github issues
     Assumes they are all New Paper issues
     Creates a data frame with doi, title, issueLink and issueLabels
@@ -105,7 +109,9 @@ def getIssuesDF(issues):
         "doi": DOIs,
         "title": titles,
         "gh_issue_link": issue_links,
-        "gh_issue_labels": lables}).set_index("doi")
+        "gh_issue_labels": lables}).set_index("doi")  
+    if removeDuplicates:
+        issuesDF = issuesDF[~issuesDF.gh_issue_labels.str.contains("duplicate")]
     return issuesDF
 
 
@@ -122,23 +128,78 @@ def getCitationsData():
     The covid19-review_paperLink is a link to that paper's citation in the html document (example: https://greenelab.github.io/covid19-review/#ref-Rt5Aik4p)
     Gets the citation info from the referneces.json file in the output branch
     """
-    citationsResponse = requests.get("https://api.github.com/repos/greenelab/covid19-review/contents/references.json?ref=output")
+    citationsResponse = requests.get("https://api.github.com/repos/greenelab/covid19-review/contents/references.json?ref=output", headers=headers)
     citations = json.loads(base64.b64decode(json.loads(citationsResponse.text)["content"]))
     citationsDF = pd.DataFrame(citations)
     citationsDF["Covid19-review_paperLink"] = citationsDF.id.apply(lambda x: "https://greenelab.github.io/covid19-review/#ref-" + x)
-    for col in citationsDF.columns:
-        print(col)
-    citationsDFforTSV = citationsDF[["DOI", "title", "issued", "container-title", "URL", "Covid19-review_paperLink"]]
-    citationsDFforTSV.rename(columns={"DOI": "doi", "issued": "date", "container-title": "publication"}, inplace=True)
-    citationsDFforTSV.set_index("doi", inplace=True)
-    return citationsDFforTSV
+    citationsDF = citationsDF[["DOI", "title", "issued", "container-title", "URL", "Covid19-review_paperLink"]]
+    citationsDF.rename(columns={"DOI": "doi", "issued": "date", "container-title": "publication"}, inplace=True)
+    
+    # Convert date to string
+    def dateStringFromDateParts(row):
+        try:
+            dateParts = row['date']['date-parts'][0]
+            if len(dateParts) == 3:
+                return "-".join([str(dateParts[1]), str(dateParts[2]), str(dateParts[0])]) 
+            elif len(dateParts) == 2:
+                return "-".join([str(dateParts[1]), str(dateParts[0])]) 
+            elif len(dateParts) == 1:
+                return str(dateParts[0])
+            else:
+                return
+        except:
+            return
+            
+    citationsDF.date = citationsDF.apply(dateStringFromDateParts, axis=1)
+    
+    citationsDF.set_index("doi", inplace=True)
+    return citationsDF
 
 
 def mergePaperDataFrames(dataFramesList):
-    # TODO: Merge the data sets, try with DOI then fuzzy match titles
+    """ Combine a list of paper dataframes into one.
     
-    # just returning the first one as a placeholder
-    return dataFramesList[0]
+    Each data frame should have the doi as index.
+    Can have title and date columns; the tile and date from the first dataframe in the list will be kept in case of conflict.
+    Any additional columns unique to dataset will be kept.
+    """
+    print(sum([len(df) for df in dataFramesList]), " total items to merge")
+    
+    # Add "_#" to title and date columns of all but the first df
+    for i in range(len(dataFramesList)):
+        if i == 0: continue
+        dataFramesList[i] = dataFramesList[i].rename(columns={"title": ("title_" + str(i)), "date": ("date_" + str(i))})
+        
+    # Seperate into items with and without valid DOIs
+    dataFramesList_DOI = []
+    dataFramesList_NoDOI = []
+    for df in dataFramesList:
+        validDOI = [str(index).startswith("10") for index in df.index]
+        invalidDOI = [False if val else True for val in validDOI]
+        dataFramesList_DOI.append(df[validDOI])
+        dataFramesList_NoDOI.append(df[invalidDOI])
+        
+    # Merge on DOIs
+    mergedOnDOI = pd.concat(dataFramesList_DOI, axis=1, sort=False)
+    mergedOnDOI['doi'] = mergedOnDOI.index
+    
+    # TODO: merge on titles
+    
+    # Add in the items that didn't have a DOI
+    dfsToMerge = dataFramesList_NoDOI
+    dfsToMerge.append(mergedOnDOI)
+    merged = pd.concat(dfsToMerge, axis=0, ignore_index=True, sort=False)
+    
+    # Combine the title and date info from duplicate columns
+    for i in range(len(dataFramesList)):
+        if i == 0: continue
+        for col in ['title', 'date']:
+            secondaryCol = col + "_" + str(i)
+            if secondaryCol in merged.columns:
+                merged[col] = merged[col].combine_first(df[secondaryCol])
+                merged.drop(secondaryCol, axis=1, inplace=True)
+        
+    return merged
 
 
 # Main
@@ -151,10 +212,10 @@ print(len(citationsData), "citations in the covid19-review paper")
 # TODO: pullRequestsData = getPullRequestData()
 
 print("\n -- merging the data --")
-combinedData = mergePaperDataFrames([issuesData, citationsData])
-print(len(combinedData), "total papers")
+combinedData = mergePaperDataFrames([citationsData, issuesData])
+print(len(combinedData), "total items after merge")
 
 # TODO: decide how and where to save the resulting TSV (output branch? main branch? via CI? via github actions?)
-combinedDataFilePath = "./covid19-reviewPaperMetaData.tsv"
+combinedDataFilePath = "./covid19-review_sourceMetaData.tsv"
 print("\n -- saving the data to ", combinedDataFilePath, " --")
 combinedData.to_csv(combinedDataFilePath, sep="\t")
