14db8a4c15fe08017b92aed7a359e2f872feef0c
Anthony Gitter <gitter@biostat.wisc.edu>
2020-04-30 22:27:39 -0500

    Update spellcheck filter

--- ci/install.sh
+++ ci/install.sh
@@ -22,5 +22,5 @@ conda activate manubot
 if [ "${SPELLCHECK:-}" = "true" ]; then
   sudo apt-get update -y
   sudo apt-get install -y aspell aspell-en
-  wget https://raw.githubusercontent.com/agitter/lua-filters/b3816d949cd2ee319068f2d38bf69cc5335aeb79/spellcheck/spellcheck.lua
+  wget https://raw.githubusercontent.com/agitter/lua-filters/19f6d037f64bf41e28b2891572fb141df24a2b8e/spellcheck/spellcheck.lua
 fi
