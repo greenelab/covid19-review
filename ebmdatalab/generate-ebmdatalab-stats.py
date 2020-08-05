1019feb6099531aec1b9856de42f692a00e5643d
HM Rando <halie.rando@pennmedicine.upenn.edu>
2020-08-05 09:41:28 -0400

    generate side-by-side choropleths

--- ebmdatalab/generate-ebmdatalab-stats.py
+++ ebmdatalab/generate-ebmdatalab-stats.py
@@ -7,6 +7,8 @@ import pandas as pd
 import urllib.request
 import geopandas
 import pycountry
+import geoplot as gplt
+from datetime import date
 
 from manubot.cite.citekey import url_to_citekey
 from manubot.cite.doi import get_short_doi_url
@@ -156,6 +158,8 @@ def main(args):
     
     fig.savefig(args.output_figure + '.png', bbox_inches = "tight")
     fig.savefig(args.output_figure + '.svg', bbox_inches = "tight")
+
+    print(f'Wrote {args.output_figure}.png and {args.output_figure}.svg')
     
     # Identify the names of each country in single-country and multi-country clinical trials
     # Multi refers to trials that have multiple country names, comma-separated
@@ -175,19 +179,34 @@ def main(args):
     # Map the ISO codes onto the country data and count the frequency
     single_countries_codes = pd.DataFrame(single_countries, index=single_countries).join(country_codes)["iso_a3"]
     single_countries_codes = single_countries_codes.dropna()
-    single_countries_counts = single_countries_codes.value_counts()
+    single_countries_counts = single_countries_codes.value_counts().rename("single_country_counts")
+
     multi_countries_codes = pd.DataFrame(multi_countries.str.strip(), index=multi_countries.str.strip()).join(country_codes)["iso_a3"]
     multi_countries_codes = multi_countries_codes.dropna()
-    multi_countries_counts = multi_countries_codes.value_counts()
+    multi_countries_counts = multi_countries_codes.value_counts().rename("multi_country_counts")
 
-    # Generate two-pane choropleth visualizing world map with number of representations in clinical trial data
+    # Map frequency data onto the geopandas geographical data
     world_data = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres')).set_index("iso_a3")
-    countries_mapping = world_data.join(pd.DataFrame(single_countries_counts)) #.join(multi_countries_counts)
-    print(countries_mapping)
-    #fig, ax = plt.subplots(1, 1)
-    #print(pd.DataFrame(multi_countries_counts, index=multi_countries_counts["iso_a3"]).join(world, on="iso_a3"))
+    countries_mapping = world_data.merge(
+        pd.DataFrame(single_countries_counts), how="inner", left_index=True, right_index=True).merge(
+        pd.DataFrame(multi_countries_counts), how="inner", left_index=True, right_index=True)
 
-    print(f'Wrote {args.output_figure}.png and {args.output_figure}.svg')
+    # Generate two-part choropleth visualizing world map with number of clinical trial data counted
+    fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(20, 16))
+    ax1.set_title("Locations of Single-Country Clinical Trials")
+    ax1 = gplt.choropleth(countries_mapping, hue = countries_mapping['single_country_counts'],
+                        legend=True, ax=ax1) #countries_mapping.plot(column='single_country_counts', ax=ax1, legend=True)
+    ax2.set_title("Locations of Multi-Country Clinical Trials")
+    ax2 = gplt.choropleth(countries_mapping, hue = countries_mapping['multi_country_counts'],
+                          legend=True, ax=ax2)
+    ax2.annotate(f'Source: EBM Data Lab COVID-19 TrialsTracker, %s' % date.today().strftime("%b-%d-%Y"),
+                 xy=(-168, -68))
+
+    plt.savefig(args.output_map + '.png', bbox_inches = "tight")
+    plt.savefig(args.output_map + '.svg', bbox_inches = "tight")
+
+    print(f'Wrote {args.output_map}.png and {args.output_map}.svg')
+    exit(0)
     
     # The placeholder will be replaced by the actual SHA-1 hash in separate
     # script after the updated image is committed
@@ -216,6 +235,11 @@ if __name__ == '__main__':
                         help='Path of the output figure with clinical trials ' \
                         'statistics without file type extension. Will be saved ' \
                         'as .png and .svg.',
+                        type=str)
+    parser.add_argument('output_map',
+                         help='Path of the output choropleth (world map figure) ' \
+                        'with geographic clinical trial frequencies, without file ' \
+                        'type extension. Will be saved as .png and .svg.',
                         type=str)
 
     args = parser.parse_args()
