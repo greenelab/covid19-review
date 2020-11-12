542a70920eaef3645abbb6ea7e862f7272dc9b6a
Anthony Gitter <gitter@biostat.wisc.edu>
2020-11-12 11:22:10 -0600

    Add new paper issue templates as appendices

---
+++ build/templates-appendix.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Uses sed command to strip Markdown frontmatter from
+# https://stackoverflow.com/questions/28221779/how-to-remove-yaml-frontmatter-from-markdown-files
+
+appendix=content/96-template-appendix.md
+
+echo "Adding diagnostics template to appendix"
+echo -e "## Appendix B {.page_break_before}\n" > $appendix
+sed '1{/^---$/!q;};1,/^---$/d' .github/ISSUE_TEMPLATE/new-diag-study-template.md >> $appendix
+
+echo "Adding therapeutics template to appendix"
+echo -e "\n## Appendix C {.page_break_before}\n" >> $appendix
+sed '1{/^---$/!q;};1,/^---$/d' .github/ISSUE_TEMPLATE/new-therapy-study-template.md >> $appendix
+
+echo "Adding general paper template to appendix"
+echo -e "\n## Appendix D {.page_break_before}\n" >> $appendix
+sed '1{/^---$/!q;};1,/^---$/d' .github/ISSUE_TEMPLATE/new-paper-template.md >> $appendix
