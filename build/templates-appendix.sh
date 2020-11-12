72422a8b0710c7b235b95d4b8294da92fa7698bc
Anthony Gitter <agitter@users.noreply.github.com>
2020-11-12 13:25:27 -0600

    Apply suggestions from code review
    Co-authored-by: HM Rando <halie.rando@pennmedicine.upenn.edu>

--- build/templates-appendix.sh
+++ build/templates-appendix.sh
@@ -5,13 +5,13 @@
 appendix=content/96-template-appendix.md
 
 echo "Adding diagnostics template to appendix"
-echo -e "## Appendix B {.page_break_before}\n" > $appendix
+echo -e "## Appendix B {.page_break_before}\nContributors were asked to complete this template to summarize and evaluate new papers related to diagnostics.\n" > $appendix
 sed '1{/^---$/!q;};1,/^---$/d' .github/ISSUE_TEMPLATE/new-diag-study-template.md | sed 's/^#*\ //' >> $appendix
 
 echo "Adding therapeutics template to appendix"
-echo -e "\n## Appendix C {.page_break_before}\n" >> $appendix
+echo -e "\n## Appendix C {.page_break_before}\nContributors were asked to complete this template to summarize and evaluate new papers related to therapeutics.\n" >> $appendix
 sed '1{/^---$/!q;};1,/^---$/d' .github/ISSUE_TEMPLATE/new-therapy-study-template.md | sed 's/^#*\ //' >> $appendix
 
 echo "Adding general paper template to appendix"
-echo -e "\n## Appendix D {.page_break_before}\n" >> $appendix
+echo -e "\n## Appendix D {.page_break_before}\nContributors were asked to complete this template to summarize and evaluate new papers related to topics besides therapeutics and diagnostics.\n" >> $appendix
 sed '1{/^---$/!q;};1,/^---$/d' .github/ISSUE_TEMPLATE/new-paper-template.md | sed 's/^#*\ //' >> $appendix
