0f344dc81e6370bc50d9661627e9ab8ce632a3c2
HM Rando <halie.rando@pennmedicine.upenn.edu>
2021-04-28 09:59:55 -0400

    add automatic reading of CORD-19 data

--- CORD-19/generate-cord19-stats.py
+++ CORD-19/generate-cord19-stats.py
@@ -4,14 +4,37 @@ from datetime import date
 import json
 import matplotlib.pyplot as plt
 import pandas as pd
+import wget
+
+def check_version(versionString):
+    """Check whether there are new updates to the CORD-19 dataset since the last time this script ran"""
+    updateDate, updateSHA1 = versionString.split(".")
+    prevRunInfo = json.load(open("CORD-19/cord19-stats.json", "r"))
+    prevSHA1 = prevRunInfo["sha1"]
+    if updateSHA1 == prevSHA1:
+        exit("CORD-19 data is up to date")
+    else:
+        #csvURL = "https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/" + updateDate + "/metadata.csv"
+        #csvFile = wget.download(csvURL, out="CORD-19/metadata.csv")
+        csvFile = "CORD-19/metadata.csv"
+        logURL = "https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/" + updateDate + "/changelog"
+        #logFile = wget.download(logURL, out="CORD-19/changelog.txt")
+        logFile = "CORD-19/changelog.txt"
+        return csvFile, logFile, updateDate, updateSHA1
 
 # Inspired by https://github.com/greenelab/meta-review/blob/master/analyses/deep-review-contrib/03.contrib-stats.ipynb
 def main(args):
+
     # Set output dictionary
     cord19_stats = dict()
 
-    # I'm not sure how to automate the downloading of a dataset from Kaggle because it requires a key which will be visible?
-    textfile = open(args.input_txt, "r")
+    # Check for updates (program will exit if no updates needed)
+    csvFile, logFile, updateDate, sha1 = check_version(args.version_info)
+    cord19_stats["update_date"] = updateDate
+    cord19_stats["sha1"] = sha1
+
+    # Parse the log file ("changelog" on the website)
+    textfile = open(logFile, "r")
     text = textfile.read()
     metadata_entries = text.split("---------------------------")
 
@@ -42,7 +65,8 @@ def main(args):
 
     corpus_stats = pd.DataFrame.from_dict(corpus_size, orient="index", columns=['size'])
 
-    paper_info = pd.read_csv(args.input_csv, usecols=[0,2,9])
+    # Parse the metadata file (csv formatted)
+    paper_info = pd.read_csv(csvFile, usecols=[0,2,9])
     paper_info["publish_time"] = pd.to_datetime(paper_info["publish_time"])
     paper_info["source_x"] = paper_info["source_x"].str.lower()
 
@@ -104,12 +128,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('input_txt',
-                        help='Path of the CORD-19 metadata readme',
-                        type=str)
-    parser.add_argument('input_csv',
-                        help='Path of the CORD-19 metadata csv file ' \
-                        'zipped',
+    parser.add_argument('version_info',
+                        help='Version info for the most recent update to the CORD-19 dataset ' \
+                             'formatted as date[space]sha1',
                         type=str)
     parser.add_argument('output_json',
                         help='Path of the JSON file with extracted statistics',
@@ -119,6 +140,5 @@ if __name__ == '__main__':
                         'statistics without file type extension. Will be saved ' \
                         'as .png and .svg.',
                         type=str)
-
     args = parser.parse_args()
     main(args)
--- CORD-19/generate-cord19-stats.sh
+++ CORD-19/generate-cord19-stats.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
-# Download EBM Data Lab COVID-19 TrialsTracker data and generate an output JSON file with relevant statistics
-# and a figure
+# Prepare to download the CORD-19 dataset and to generate an output JSON file with relevant statistics
+# and a figure using the associated Python script
 
-# Specify the relative path to the file in the repo
-# EBM_REPO_PATH=notebooks%2Fwebsite_data%2Ftrials_latest.json
+# Get the current release information
+#wget https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/historical_releases.html
 
-# Then get the most recent commit affecting this file and store the commit sha and date
-# See https://stackoverflow.com/questions/50194241/get-when-the-file-was-last-updated-from-a-github-repository/50204589
-# EBM_COMMIT_JSON=$(curl -sS "https://api.github.com/repos/ebmdatalab/covid_trials_tracker-covid/commits?path=$EBM_REPO_PATH&per_page=1")
-# export EBM_COMMIT_SHA=$(echo $EBM_COMMIT_JSON | python -c "import sys, json; print(json.load(sys.stdin)[0]['sha'])")
-# export EBM_COMMIT_DATE=$(echo $EBM_COMMIT_JSON | python -c "import sys, json; print(json.load(sys.stdin)[0]['commit']['author']['date'])")
+# Parse the release info to pull the most recent date and SHA (separated by a .)
+CORD19_VERSION=$(python CORD-19/parse_html.py)
 
-CORD19_INPUT_TXT=CORD-19/metadata.readme
-CORD19_INPUT_CSV=CORD-19/metadata.csv.zip
+# Set the filenames/locations of the output files
 CORD19_STATS_JSON=CORD-19/cord19-stats.json
 CORD19_FIG=CORD-19/cord19-growth
 
-# I'm not sure how to automate the downloading of a dataset from Kaggle because it requires a key which will be visible?
-# echo "Downloading CORD-19 data" # from commit $EBM_COMMIT_SHA authored $EBM_COMMIT_DATE"
-# wget https://www.kaggle.com/allen-institute-for-ai/CORD-19-research-challenge/download > kaggle.json
-# kaggle datasets download -d allen-institute-for-ai/CORD-19-research-challenge
-
 # After running this Python script to generate the figures, commit the figures
-# and run the version-figures.sh script to update the EBM_STATS_JSON with the
+# and run the version-figures.sh script to update the CORD-19 figures with the
 # versioned figure URL
-# echo "Generating EBM Data Lab COVID-19 TrialsTracker statistics and figure"
-# python ebmdatalab/generate-ebmdatalab-stats.py $EBM_INPUT_JSON $EBM_STATS_JSON $EBM_FIG $EBM_MAP
-python CORD-19/generate-cord19-stats.py $CORD19_INPUT_TXT $CORD19_INPUT_CSV $CORD19_STATS_JSON $CORD19_FIG
-# rm $EBM_INPUT_JSON
+
+echo "Generating CORD-19 statistics and figure"
+python CORD-19/generate-cord19-stats.py $CORD19_VERSION $CORD19_STATS_JSON $CORD19_FIG
+
+# add cleanup
+#rm historical_releases.html
+#rm metadata.csv 2> /dev/null
+#rm changelog.txt 2> /dev/null
