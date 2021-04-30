7ef6fdaae1adff097fbaac9f20ed795628d836e7
HM Rando <halie.rando@pennmedicine.upenn.edu>
2021-04-30 10:02:06 -0400

    add tracking of manuscript statistics

---
+++ analyze-ms-stats/calc-manuscript-stats.sh
@@ -0,0 +1,15 @@
+# Calculate growth statistics for manuscript based on Manubot files
+
+# Generate list of all commits in history of output branch
+git log --pretty=format:"%h" > output-commits.txt
+
+# Define input and output files
+COMMIT_LIST=./output-commits.txt
+OUTPUT_JSON=manuscript_stats.json
+OUTPUT_FIG=manuscript_stats
+
+# Run python script
+python calc-manuscript-stats.py $COMMIT_LIST $OUTPUT_JSON $OUTPUT_FIG
+
+# Clean up temporary files
+rm ./references_tmp.json ./variables_tmp.json
