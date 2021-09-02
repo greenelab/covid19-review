f966e34dfc3b46160848f7e164bdd63b96add1d7
HM Rando <halie.rando@pennmedicine.upenn.edu>
2021-09-02 18:02:49 -0400

    cache data from previous commits

--- analyze-ms-stats/calc-manuscript-stats.py
+++ analyze-ms-stats/calc-manuscript-stats.py
@@ -1,13 +1,15 @@
 import json
-import subprocess
 import pandas as pd
 import matplotlib
 import argparse
+import time
+import os.path
 import multiprocessing
+import subprocess
 
 def analyze_commit(commit):
     """Access files and data in variables.json associated with each commit
-    Accepts commit ID # as string
+    Accepts commit ID as string
     Returns list of 5 statistics"""
 
     variablesCommand = "git show " + commit + ":./variables.json"
@@ -28,20 +30,44 @@ def analyze_commit(commit):
 def main(args):
     '''Extract statistics from the output branch log'''
 
-    # Access the variables.json and references.json files associated with each commit and store in dictionary
+    start_time = time.time()
+    print(f"Using {multiprocessing.cpu_count()} CPUs")
+
+    # Read in list of all commits on this branch
     with open(args.commit_list, "r") as commitFile:
         commits = [c.strip() for c in commitFile.read().splitlines()]
-        pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
-        commitData = dict(zip(commits, pool.map(analyze_commit, commits)))
-        pool.close()
-        pool.join()
 
-    # Convert dictionary to dataframe & flip index to be chronological
+    # If this analysis has been run before, load in the list of commits analyzed
+    # and only analyze new commits
+    # Assumes no commits will be added retrospectively (to take advantage of linearity)
+    priorData = None
+    if os.path.exists(args.output_table):
+        priorData = pd.read_csv(args.output_table)
+        oldCommits = priorData["commit"].tolist()
+        priorData = priorData.set_index("commit")
+
+        if len(commits) > len(oldCommits):
+            start_old = commits.index(oldCommits[0])
+            commits = commits[:start_old]
+            print(f"{len(commits)} new commits")
+        else:
+            exit("No new commits")
+
+    # Access the variables.json and references.json files associated with each commit and store in dictionary
+    pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+    commitData = dict(zip(commits, pool.map(analyze_commit, commits)))
+    pool.close()
+    pool.join()
+
+    # Turn commitData to df, then flip to be in chronological order
     growthdata = pd.DataFrame.from_dict(commitData, orient="index",
                                         columns=["Date", "clean_date", "Authors", "Word Count", "References"])
-    manuscript_stats = growthdata.iloc[0].to_dict()
-    for item in ["Authors", "Word Count", "References"]:
-        manuscript_stats[item] = str(manuscript_stats[item])
+
+    # Append onto table of previous commit data, if this exists
+    if priorData is not None:
+        growthdata = pd.concat([growthdata, priorData])
+
+    # Reverse data to match chronological time
     growthdata = growthdata[::-1]
     growthdata = growthdata.set_index("Date")
 
@@ -63,9 +89,21 @@ def main(args):
     print(f'Wrote {args.output_figure}.png and {args.output_figure}.svg')
 
     # Write json output file
+    manuscript_stats = growthdata.iloc[0].to_dict()
+    for item in ["Authors", "Word Count", "References"]:
+        manuscript_stats[item] = str(manuscript_stats[item])
     with open(args.output_json, 'w') as out_file:
         json.dump(manuscript_stats, out_file, indent=2, sort_keys=True)
     print(f'Wrote {args.output_json}')
+
+    # Cache commit data for future updates
+    pd.DataFrame.from_dict(commitData, orient='index',
+                           columns=["date", "clean_date",
+                                    "num_authors", "word_count", "num_ref"])\
+        .to_csv(args.output_table, index_label="commit")
+    print(f'Wrote {args.output_table}')
+
+    print("Time:", time.time() - start_time)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -81,6 +119,10 @@ if __name__ == '__main__':
                         help='Path of the output figure for manuscript ' \
                         'statistics without file type extension. Will be saved ' \
                         'as .png and .svg.',
+                        type=str)
+    parser.add_argument('output_table',
+                        help='Path of the output table used to generate ' \
+                        'figures ',
                         type=str)
     args = parser.parse_args()
     main(args)
--- analyze-ms-stats/calc-manuscript-stats.sh
+++ analyze-ms-stats/calc-manuscript-stats.sh
@@ -2,16 +2,17 @@
 
 # Generate list of all commits in history of output branch
 echo "Generate log for output branch"
-git log --pretty=format:"%h" --first-parent output > analyze-ms-stats/output-commits.txt
+git log --pretty=format:"%h" --first-parent upstream/output > analyze-ms-stats/output-commits.txt
 
 # Define input and output files
 COMMIT_LIST=analyze-ms-stats/output-commits.txt
 OUTPUT_JSON=analyze-ms-stats/manuscript_stats.json
 OUTPUT_FIG=content/images/manuscript_stats
+OUTPUT_TABLE=analyze-ms-stats/commitData.csv
 
 # Run python script
 echo "Run python script"
-python analyze-ms-stats/calc-manuscript-stats.py $COMMIT_LIST $OUTPUT_JSON $OUTPUT_FIG
+python analyze-ms-stats/calc-manuscript-stats.py $COMMIT_LIST $OUTPUT_JSON $OUTPUT_FIG $OUTPUT_TABLE
 
 # Clean up temporary files
 echo "Clean up temporary files"
